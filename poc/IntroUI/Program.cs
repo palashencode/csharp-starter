b469c7442ec1555b965ecf84d90251680e1f3951
Palash <palash.nandi42@gmail.com>
2020-11-29 22:08:51 +0530

    C Sharp Starter for VSCode

---
+++ poc/IntroUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel{
+                FirstName = "Tim",
+                LastName = "Corey"
+            };
+
+            System.Console.WriteLine($"{ p.FirstName } { p.LastName } is my name.");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a CSharp test");
+            Console.WriteLine("End Line.");
+        }
+    }
+}
---
+++ poc/README.md
@@ -0,0 +1,21 @@
+
+# CSharp Visual Studio Starter Project
+
+**Commands:**
+---
+
+- `dotnet new sln -n "VSCodeSln"` 
+- `dotnet new console -n "IntroUI"`
+- `dotnet new classlib -n "IntroLibrary"`
+- `dotnet sln VSCodeSln.sln add **/*.csproj`
+- `dotnet add IntroUI/IntroUI.csproj reference IntroLibrary/IntroLibrary.csproj`
+
+**Execute Code**
+---
+
+- `cd IntroUI` , `dotnet run`
+
+**Adding Nuget Packages:**
+---
+
+- `dotnet add package Dapper`
